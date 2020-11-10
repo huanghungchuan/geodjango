df14adc62760bd0a6c8b4dad04c1ccf27d0e7305
huanghungchuan <huanghungchuan@gmail.com>
2020-11-10 23:54:52 +0000

    update create_docker.sh

--- create_docker.sh
+++ create_docker.sh
@@ -1,9 +1,29 @@
-docker stop geodjango_nginx
-docker stop geodjango_container
-docker rm geodjango_container
-docker rmi geodjango
-docker build -t geodjango .
-docker create --name geodjango_container --network geodjango_network --network-alias geodjangoImage -t -p 28001:8001 geodjango
-docker start geodjango_container
-docker create --name geodjango_nginx --network geodjango_network --network-alias geodjangoNginx -t -p 20080:80 -v volume_Name:/usr/share/nginx/html nginx
-docker start geodjango_nginx
+#!/bin/bash
+
+# Createing PGAdmin4 Container
+sudo docker stop geodjango_pgadmin4
+sudo docker rm geodjango_pgadmin4
+sudo docker create --name geodjango_pgadmin4 --network wmap_network --network-alias wmap-pgadmin4 -t -v wmap_pgadmin_data:/var/lib/pgadmin -e 'PGADMIN_DEFAULT_EMAIL=hugnchuan.huang@tudublin.ie' -e 'PGADMIN_DEFAULT_PASSWORD=mypassword' dpage/pgadm$
+sudo docker start geodjango_pgadmin4
+
+# Createing PostGIS Container
+sudo docker stop geodjango_postgis
+sudo docker rm geodjango_postgis
+sudo docker create --name wmap_postgis --network wmap_network --network-alias wmap-postgis -t -v wmap_postgis_data:/var/lib/postgresql kartoza/postgis
+sudo docker start geodjango_postgis
+
+# Creating Project Container
+sudo docker stop geodjango_container
+sudo docker rm geodjango_container
+sudo docker rmi geodjango
+sudo docker build -t geodjango .
+sudo docker create --name geodjango_container --network geodjango_network --network-alias geodjangoImage -t -p 28001:8001 geodjango
+sudo docker start geodjango_container
+
+# Creating NginX Container
+sudo docker stop geodjango_nginx
+sudo docker rm geodjango_nginx
+sudo docker rmi nginx_image
+sudo docker build -t nginx_image ./nginx
+sudo docker create --name geodjango_nginx --network wmap_network --network-alias wmap-nginx-certbot -p 80:80 -p 443:443 -t -v wmap_web_data:/usr/share/nginx/html -v $HOME/wmap_nginx_certbot/conf:/etc/nginx/conf.d -v /etc/letsenc$
+sudo docker start geodjango_nginx
