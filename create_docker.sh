16cb226f1737fa2bb5475d106c91381b9e4f431d
huanghungchuan <huanghungchuan@gmail.com>
2020-11-11 00:13:07 +0000

    update create_docker.sh

--- create_docker.sh
+++ create_docker.sh
@@ -3,13 +3,13 @@
 # Createing PGAdmin4 Container
 sudo docker stop geodjango_pgadmin4
 sudo docker rm geodjango_pgadmin4
-sudo docker create --name geodjango_pgadmin4 --network wmap_network --network-alias wmap-pgadmin4 -t -v wmap_pgadmin_data:/var/lib/pgadmin -e 'PGADMIN_DEFAULT_EMAIL=hugnchuan.huang@tudublin.ie' -e 'PGADMIN_DEFAULT_PASSWORD=mypassword' dpage/pgadm$
+sudo docker create --name geodjango_pgadmin4 --network geodjango_network --network-alias wmap-pgadmin4 -t -v wmap_pgadmin_data:/var/lib/pgadmin -e 'PGADMIN_DEFAULT_EMAIL=hugnchuan.huang@tudublin.ie' -e 'PGADMIN_DEFAULT_PASSWORD=mypassword' dpage/pgadm$
 sudo docker start geodjango_pgadmin4
 
 # Createing PostGIS Container
 sudo docker stop geodjango_postgis
 sudo docker rm geodjango_postgis
-sudo docker create --name wmap_postgis --network wmap_network --network-alias wmap-postgis -t -v wmap_postgis_data:/var/lib/postgresql kartoza/postgis
+sudo docker create --name geodjango_postgis --network geodjango_network --network-alias wmap-postgis -t -v wmap_postgis_data:/var/lib/postgresql kartoza/postgis
 sudo docker start geodjango_postgis
 
 # Creating Project Container
@@ -25,5 +25,11 @@ sudo docker stop geodjango_nginx
 sudo docker rm geodjango_nginx
 sudo docker rmi nginx_image
 sudo docker build -t nginx_image ./nginx
-sudo docker create --name geodjango_nginx --network wmap_network --network-alias wmap-nginx-certbot -p 80:80 -p 443:443 -t -v wmap_web_data:/usr/share/nginx/html -v $HOME/wmap_nginx_certbot/conf:/etc/nginx/conf.d -v /etc/letsenc$
+sudo docker create --name geodjango_nginx --network geodjango_network --network-alias na_nginx \
+-p 80:80 -p 443:443 -t \
+-v wmap_web_data:/usr/share/nginx/html \
+-v /etc/letsencrypt:/etc/letsencrypt \
+-v /var/www/certbot \
+-v html_data:/usr/share/nginx/html/static \
+nginx_image
 sudo docker start geodjango_nginx
